2f71084b73a13b9e4eae395f89238cef149ffd11
Sasianandh <31653406+Sasianandh@users.noreply.github.com>
2018-03-18 16:50:48 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -3,7 +3,7 @@
 int main()
 {
    char a[100],t;
-   int i,c,b=0,d;
+   int i,c,b=0,d,k=1;
   gets(a);
    c=strlen(a);
 for(i=0;i<c;i++)
@@ -11,23 +11,40 @@ for(i=0;i<c;i++)
 t=a[i];
     switch(t)
     {
-        case 'I':
+        case 'i': case 'I':
         d=1;
         break;
-        case 'V':
+        case 'v': case 'V':
         d=5;
         break;
-        case 'X':
-        d=9;
+        case 'x': case 'X':
+        d=10;
+        break;
+        case 'l': case 'L':
+        d=50;
+        break;
+        case 'c': case 'C':
+        d=100;
+        break;
+        case 'd': case 'D':
+        d=500;
+        break;
+        case 'm': case 'M':
+        d=1000;
         break;
         default:
         printf("error");
         break;
         }
-        if(i>=1&&b<d)
-        b=b+d-2;
+        if(i>=1&&k<d)
+        {
+        b=b+(d-k-b);
+        }
         else
+        {
         b=b+d;
+        }
+        k=d;
 }
 printf("%d",b);
 return 0;
