7a843f01ac716493d8e67ba477df54484a674de8
Sasianandh <31653406+Sasianandh@users.noreply.github.com>
2018-03-18 13:11:46 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,29 +1,34 @@
 #include<stdio.h>
-void main()
+#include<string.h>
+int main()
 {
-   char a[100],d;
-   int i,c,b=0;
-   gets(a);
+   char a[100],t;
+   int i,c,b=0,d;
+  gets(a);
    c=strlen(a);
 for(i=0;i<c;i++)
 {
-    d=a[i];
-    switch(d)
+t=a[i];
+    switch(t)
     {
-        case I:
+        case 'I':
         d=1;
         break;
-        case V:
+        case 'V':
         d=5;
         break;
-        case X:
+        case 'X':
         d=9;
         break;
         default:
         printf("error");
+        break;
         }
+        if(i>=1&&b<d)
+        b=b+d-2;
+        else
         b=b+d;
 }
 printf("%d",b);
+return 0;
 }
-   
