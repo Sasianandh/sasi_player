aa4dbc2736551db782f9d3858e82aad1300cafa3
Sasianandh <31653406+Sasianandh@users.noreply.github.com>
2018-03-25 21:13:26 +0530

    Update 12.c

--- 12.c
+++ 12.c
@@ -3,7 +3,7 @@
 void main()
 {
     int a[100];
-    int n,k,i,t;
+    int n,k,i,t,j;
     scanf("%d%d",&n,&k);
     for(i=0;i<n;i++)
     {
@@ -11,12 +11,15 @@ void main()
     }
     for(i=0;i<k;i++)
     {
-        t=a[i+1];
-        a[i+1]=a[i];
-        a[i]=t;
+        t=a[n-1];
+        for(j=n-1;j>0;j--)
+        {
+            a[j]=a[j-1];
+        }
+        a[0]=t;
     }
     for(i=0;i<n;i++)
     {
         printf("%d",a[i]);
-}
+    }
 }
