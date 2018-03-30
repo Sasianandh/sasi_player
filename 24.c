66702061acea402b517d0eff134d4031f9f93287
Sasianandh <31653406+Sasianandh@users.noreply.github.com>
2018-03-30 11:15:37 +0530

    Update 24.c

--- 24.c
+++ 24.c
@@ -1,29 +1,25 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<string.h>
 void main()
 {
-   char s[100];
-   int i=0,c,j,d=0,v=0;
-   gets(s);
-   c=strlen(s);
-   while(i<c)
-   {
-       for(j='0';j<='9';j++)
-       {
-           if(s[i]!=s[j])
-           {
-           d++;
-           }
-       }
-       if(d>0)
-       {
-           printf("no");
-         v++;
-       break;
-       }
-       else
-       i++;
-       }
-       if(v==0)
-       printf("yes");
-   }
+ char s[100],j;
+ int c,i,d=0,e=0;
+ gets(s);
+ c=strlen(s);
+ for(i=0;i<c;i++)
+ {
+    for(j='0';j<='9';j++)
+    {
+    if(s[i]==j)
+    d++;
+    }
+    if(d==0)
+    e++;
+    else
+    d=0;
+    }
+    if(e==0)
+    printf("yes it is numeric");
+    else
+    printf("no");
+}
